00a41c0321cd9267450aa5f7b117a9f62ed2b776
Lucas31edu <148974241+Lucas31edu@users.noreply.github.com>
2023-11-22 09:25:44 -0300

    Atualizar o script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+function openTab(tabName) {
+    var i, tabContent;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    document.getElementById(tabName + "Content").style.display = "block";
+}
